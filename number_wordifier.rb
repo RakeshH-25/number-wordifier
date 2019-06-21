5e934bdb06dc7a7b701989c8c8c0ad8d7684b202
Rakesh H <rakesh.h@v2solutions.com>
2019-06-21 10:31:21 +0530

    Improvising number to word converter logic.

--- number_wordifier.rb
+++ number_wordifier.rb
@@ -2,13 +2,17 @@ class NumberWordifier
   require './lib/dictionary.rb'
 
   def convert number
+    start_time = Time.now
     number = number.tr('^2-9', '') #Removes alphabets and 0,1
     if valid_number?(number)
       all_matching_words_from_dictionary = word_combinations(number)
       one_word_possibilities = all_matching_words_from_dictionary.select{|ele| ele.length == 10}
-      p multiple_word_possibilities = combine_words(all_matching_words_from_dictionary)
+      multiple_word_possibilities = combine_words(all_matching_words_from_dictionary)
+      word_combinations = one_word_possibilities + multiple_word_possibilities
+      p "Time taken : #{Time.now - start_time}"
+      word_combinations
     else
-      p "invalid Number"
+      p "Invalid Number"
     end
   end
 
@@ -30,6 +34,8 @@ class NumberWordifier
   def combine_words matching_words
     # Construct 10 letter word combination from the words matching/filtered.
     combination_array = (2..3).flat_map{|n| matching_words.combination(n).to_a} # Max 3 words can be combined for an 10 digit number
+    combination_array = combination_array.select{|arr| arr.join.length == 10} # select combinations with length 10
+    combination_array.map{|ele| ele.join(',')} #join to form a single
 
   end
 end
